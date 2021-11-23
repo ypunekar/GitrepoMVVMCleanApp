e1c4c8bf1d9b582d81378f156a23d2bd01aa6426
ypunekar <punekaryogita@gmail.com>
2021-11-23 16:49:04 +0530

    Added jetpack compose changes to display list.

--- app/src/main/java/com/example/gitrepomvvmcleanapp/presentation/feed/FeedActivity.kt
+++ app/src/main/java/com/example/gitrepomvvmcleanapp/presentation/feed/FeedActivity.kt
@@ -1,17 +1,26 @@
 package com.example.cleanarchpoc.presentation.feed
 
 import android.content.ContentValues.TAG
+import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProviders
@@ -38,35 +47,14 @@ class FeedActivity : BaseActivity<FeedViewModel>() {
         daggerInjector.createFeedComponent().inject(this)
        super.onCreate(savedInstanceState)
         setContent {
-
+            viewModel.loadCountries()
             val recipes = viewModel.getCountrisLiveData().value
 
             for(recipe in recipes){
                 Log.d(TAG, "RECIPE: ${recipe.projectName}")
             }
+            AllPlants(recipes)
 
-            Column(modifier = Modifier.padding(16.dp)) {
-                Text(
-                    text = "RecipeList",
-                    style = TextStyle(
-                        fontSize = TextUnit.Companion.Unspecified
-                    )
-                )
-                Text(
-                    text = "RecipeList",
-                    style = TextStyle(
-                        fontSize = TextUnit.Companion.Unspecified
-                    )
-                )
-                Spacer(modifier = Modifier.padding(10.dp))
-                Button(
-                    onClick = {
-                       viewModel.loadCountries()
-                    }
-                ) {
-                    Text(text = "PERFORM SEARCH")
-                }
-            }
         }
        // viewModel.loadCountries()
 
@@ -95,5 +83,51 @@ class FeedActivity : BaseActivity<FeedViewModel>() {
 
 }
 
+@Composable
+fun AllPlants (platList: List<Repositories>){
+    Scaffold(
+    ) {
+        LazyColumn(
+            Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            items(platList){plant ->
 
+                plant.Description?.let { it1 -> PlantCard(plant.projectName, it1) }
+            }
+        }
+    }
+}
+@Composable
+fun PlantCard(name: String, description: String) {
+    Card(
+        modifier = Modifier
+            // The space between each card and the other
+            .padding(10.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        shape = MaterialTheme.shapes.medium,
+        elevation = 5.dp,
+        backgroundColor = MaterialTheme.colors.surface
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Column(Modifier.padding(8.dp)) {
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.subtitle1,
+                    modifier = Modifier.padding(bottom = 8.dp).fillMaxWidth(),
+                    color = Color.Black
+                    )
+                Text(
+                    text = description,
+                    color = Color.Gray,
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
+            }
+        }
+    }
+}
 
